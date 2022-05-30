a260906e9fceb2f1195ba3e2506291ffc63e1332
0p4tr1k0 <BeruJenHotovostZRukyDoRuky@seznam.cz>
2022-05-30 21:05:11 +0200

    První verze 5.0

--- myfirst/admin.py
+++ myfirst/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Types)
+admin.site.register(Attachment)
 admin.site.register(Tank)
--- myfirst/models.py
+++ myfirst/models.py
@@ -1,20 +1,11 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
-
-class Types(models.Model):
-    name = models.CharField(max_length=50, unique=True, verbose_name='Tank type', help_text='Enter Tank type')
-    abbr = models.CharField(max_length=3, unique=True, validators=[MinLengthValidator(2)], verbose_name='Tank type Abbreviation',
-                            help_text='Enter Abbreviation')
-
-    class Meta:
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name
+def img_path(instance, filename):
+    return "tanky/" + str(instance.attachmentid) + "/img/" + filename
 
 
 class Tank(models.Model):
-    #charfield je varchar, integer field je int
+    # charfield je varchar, integer field je int
     HT = 'Heavy'
     MT = 'Medium'
     LT = 'Light'
@@ -41,30 +32,42 @@ class Tank(models.Model):
         ('POLAND', 'POLAND'),
         ('ITALY', 'ITALY'),
     ]
-    DEPLOYMENT = [
-        ('FRONTLINE', 'FRONTLINE'),
-        ('REAR', 'REAR'),
+    TIER = [
+        ('I', '1'),
+        ('II', '2'),
+        ('III', '3'),
+        ('IV', '4'),
+        ('V', '5'),
+        ('VI', '6'),
+        ('VII', '7'),
+        ('VIII', '8'),
+        ('IX', '9'),
+        ('X', '10'),
     ]
 
+    tankid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True, blank=True, null=True, verbose_name='Tank name',
+                            help_text='Enter Tank name')
+    types = models.CharField(max_length=50, choices=TYPES, blank=True, null=True, verbose_name='Type of tank')
+    nations = models.CharField(max_length=50, choices=NATIONS, blank=True, null=True, verbose_name='Tank nation')
+    tier = models.CharField(max_length=50, choices=TIER, blank=True, null=True, verbose_name='Level of tank')
 
-    name = models.CharField(max_length=50, unique=True, verbose_name='Tank name', help_text='Enter Tank name')
-    types = models.CharField(max_length=50, choices=TYPES, verbose_name='Type of tank')
-    tank_class = models.CharField(max_length=50, choices=NATIONS, verbose_name='Choose nation')
-    deploy = models.CharField(max_length=50, choices=DEPLOYMENT, verbose_name='Choose deployment', help_text='Where was the tank deployed')
+    def __str__(self):
+        return self.name
 
 
+class Attachment(models.Model):
+    attachmentid = models.AutoField(primary_key=True)
+    tank = models.ForeignKey(Tank, models.DO_NOTHING, blank=True, null=True, verbose_name='idk')
+    name = models.CharField(max_length=200, verbose_name='Tank info', blank=True, null=True,
+                            help_text='Enter tank information')
+    abbr = models.CharField(max_length=3, unique=True, validators=[MinLengthValidator(2)],
+                            verbose_name='Tank type Abbreviation',
+                            help_text='Enter Abbreviation')
+    image = models.ImageField(upload_to=img_path, blank=True, null=True, verbose_name="Image")
 
     class Meta:
         ordering = ['name']
 
     def __str__(self):
-            return self.name
-
-class Patch(models.Model):
-    patch_class = models.CharField(max_length=50, unique=True, verbose_name='Patch', help_text='Verze hry')
-    patch_dor = models.DateField
-
-
-    def __str__(self):
-            return self.name
-
+        return self.name
