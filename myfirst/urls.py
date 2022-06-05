059a56d9211018df28c47f1c888c872576a6f086
0p4tr1k0 <BeruJenHotovostZRukyDoRuky@seznam.cz>
2022-06-05 21:08:33 +0200

    stránka se seznamem

--- myfirst/urls.py
+++ myfirst/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 path('', views.index, name='index'),
+path('tanks/', views.TankListView.as_view(), name='tanks'),
+
 ]
--- myfirst/views.py
+++ myfirst/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from myfirst.models import Tank
+from django.views.generic import ListView
 
 
 def index(request):
-    tanks = Tank.objects.all()
+    tanks = Tank.objects.all().order_by('nations')[:3]
     context={
         "tanks":tanks
     }
     return render(request, 'index.html', context=context)
+
+class TankListView(ListView):
+ # Nastavení požadovaného modelu
+ model = Tank
+ context_object_name = 'tank_list'
+ # Umístění a název šablony
+ template_name = 'list.html'
